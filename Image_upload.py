f75ac04ef4885278c0f2d241d848cb46fd0c59f5
Makesh-Srinivasan <66047630+Makesh-Srinivasan@users.noreply.github.com>
2021-06-29 21:28:49 +0530

    Update Image_upload.py

--- Image_upload.py
+++ Image_upload.py
@@ -1,3 +1,4 @@
+# DO NOT MODIFY THE CODE
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
@@ -9,33 +10,38 @@ import webbrowser
 
 def submit():
     img = e1.get()
-    image = PIL.Image.open(img, 'r')
-    data = ''
-    imgdata = iter(image.getdata())
-    while (True):
-            pixels = [value for value in imgdata.__next__()[:3] +
-                                    imgdata.__next__()[:3] +
-                                    imgdata.__next__()[:3]]
-
-            binstr = ''
-
-            for i in pixels[:8]:
-                if (i % 2 == 0):
-                    binstr += '0'
-                else:
-                    binstr += '1'
-
-            data += chr(int(binstr, 2))
-            if (pixels[-1] % 2 != 0):
-                    print(data)
-                    e3 = tk.Label(master,text = data,fg = '#015efe')
-                    e3.grid(row=5,column=0)
-                    e3.bind("<Button-1>", lambda e: webbrowser.open_new_tab(data))
-                    tk.Label(master, text="******PLEASE CLICK THE ABOVE LINK*****").grid(row=7,column=0)
-                    break
+    if e1.get()[-3:] == 'png':
+        image = PIL.Image.open(img, 'r')
+        data = ''
+        imgdata = iter(image.getdata())
+        while (True):
+                pixels = [value for value in imgdata.__next__()[:3] + imgdata.__next__()[:3] + imgdata.__next__()[:3]]
+                binstr = ''
+                for i in pixels[:8]:
+                    if (i % 2 == 0):
+                        binstr += '0'
+                    else:
+                        binstr += '1'            
+                data += chr(int(binstr, 2))
+                if (pixels[-1] % 2 != 0):
+                        print(data)
+                        if(len(data)!=84):
+                            print(data)
+                            tk.Label(master,text="You are doing the right thing but using the wrong thing",fg='#ff0000',bg = '#0ff0fc').grid(row=7,column=0)
+                            break
+                        e3 = tk.Label(master,text = data,fg = '#015efe',bg='#fbff00')
+                        e3.grid(row=5,column=0)
+                        e3.bind("<Button-1>", lambda e: webbrowser.open_new_tab(data))
+                        tk.Label(master, text="Welcome to round 3, you have been given a task already, complete it to go to the next round. All the best on your quest",fg='#ff0000',bg='#0ff0fc').grid(row=7,column=0)
+                        break
+    else:
+        tk.Label(master,text="You are doing the right thing but using the wrong thing",fg='#ff0000',bg = '#0ff0fc').grid(row=7,column=0)
+        
   
 master = tk.Tk()
-tk.Label(master, text="Enter Image Path").grid(row=0,column=0)
+master.configure(bg='#0ff0fc')
+master.title("WELCOME TO CODECHEF -> ROUND-3")
+tk.Label(master, text="Who sent you to this portal ?!  Where can I find your crest?",bg='#fc0dcc').grid(row=0,column=0)
 
 e1 = tk.Entry(master,width = 100)
 
@@ -45,14 +51,9 @@ e1.grid(row=0, column=1)
 
 tk.Button(master, 
           text='SUBMIT', 
-          command=submit).grid(row=3, 
-                                    column=0, 
-                                    sticky=tk.W, 
-                                    pady=4)
+          command=submit,bg='#2ac778').grid(row=3, column=0, sticky=tk.W,pady=4)
 
 
 
 
 tk.mainloop()
-# Who sent you to this portal?! Show me the crest you bear - add the path to the image file here     You have been given access! Welcome to round 3, you have been given a task already, complete it to go to the next round. All the best on your quest
-# if wrong file is uploaded the message "you are doing the right thing but using the wrong thing"
